8e6e66421aec7fe6203b8248e2da95e67d7cf82b
Yusuke Manaka <y-manaka@sip-mi.t.u-tokyo.ac.jp>
2020-08-31 16:24:24 +0900

    SSH版の実行ディレクトリ名をAPIにあわせて、UUIDとした。そのための修正と外部計算機での実行プログラムの名称をより一般的なものへ変更

--- scripts/create_inp_file_api.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#cd /tmp/node_calc; python ~/abaqus_scripts/create_inputdata.py Modeldata.inp ~/abaqus_scripts/Cyclic_SSCurve_csv; mv XX_inp XX.inp > /tmp/node_calc/node_calc_exec.log 2>&1
-#cd /tmp/node_calc
-python ~/abaqus_scripts/create_sscurve.py yeild_strength.dat tesile_strength.dat reduction_area.dat
-python ~/abaqus_scripts/create_inputdata.py ~/input_data/Modeldata.inp Cyclic_SSCurve_csv
-mv XX_inp XX.inp
-/usr/local/bin/abaqus Job=XX interactive cpus=4 interactive mp_mode=threads
