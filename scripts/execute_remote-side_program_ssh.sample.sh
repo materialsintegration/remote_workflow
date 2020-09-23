1686086d690ce3bda540889b8fefe4504023733b
Yusuke Manaka <y-manaka@sip-mi.t.u-tokyo.ac.jp>
2020-09-23 18:20:22 +0900

    より汎用的なファイル構成への対応

--- scripts/execute_remote-side_program_ssh.sample.sh
+++ scripts/execute_remote-side_program_ssh.sample.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python2 ~/misrc_remote_workflow/scripts/remote-side_scripts/create_sscurve.py yeild_strength.dat tesile_strength.dat reduction_area.dat
-python2 ~/misrc_remote_workflow/scripts/remote-side_scripts/create_inputdata.py ~/remote_workflow/scripts/input_data/Modeldata.inp Cyclic_SSCurve_csv
+python2 ~/misrc_remote_workflow/scripts/remote-side_scripts/create_inputdata.py ~/misrc_remote_workflow/scripts/input_data/Modeldata.inp Cyclic_SSCurve_csv
 mv XX_inp XX.inp
