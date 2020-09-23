c5403a5560b973e53223e1abc01948c583ba4ffd
Yusuke Manaka <y-manaka@sip-mi.t.u-tokyo.ac.jp>
2020-09-23 17:29:25 +0900

    より汎用的なファイル構成への対応

--- scripts/execute_remote-side_program_api.sample.sh
+++ scripts/execute_remote-side_program_api.sample.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #cd /tmp/node_calc
-python3 ~/remote_workflow/scripts/abaqus_scripts/create_sscurve.py yeild_strength.dat tesile_strength.dat reduction_area.dat
-python3 ~/remote_workflow/scripts/abaqus_scripts/create_inputdata.py ~/remote_workflow/scripts/input_data/Modeldata.inp Cyclic_SSCurve_csv
+python3 ~/remote_workflow/scripts/remote-side_scripts/create_sscurve.py yeild_strength.dat tesile_strength.dat reduction_area.dat
+python3 ~/remote_workflow/scripts/remote-side_scripts/create_inputdata.py ~/remote_workflow/scripts/input_data/Modeldata.inp Cyclic_SSCurve_csv
 mv XX_inp XX.inp
 /usr/local/bin/abaqus Job=XX interactive cpus=4 interactive mp_mode=threads
