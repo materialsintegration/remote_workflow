841a7a9a888c7da884d47f27feecf29715702f89
Yusuke Manaka <y-manaka@sip-mi.t.u-tokyo.ac.jp>
2020-10-07 17:04:10 +0900

    微修正

--- scripts/execute_remote-side_program_ssh.sample.sh
+++ scripts/execute_remote-side_program_ssh.sample.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-/usr/local/bin/abaqus Job=XX interactive cpus=4 interactive mp_mode=threads
 python2 ~/misrc_remote_workflow/scripts/remote-side_scripts/create_sscurve.py yeild_strength.dat tesile_strength.dat reduction_area.dat
 python2 ~/misrc_remote_workflow/scripts/remote-side_scripts/create_inputdata.py ~/misrc_remote_workflow/scripts/input_data/Modeldata.inp Cyclic_SSCurve_csv
 mv XX_inp XX.inp
+/usr/local/bin/abaqus Job=XX interactive cpus=4 interactive mp_mode=threads
