980a4af2605fc4a20ab6a3094b58a90cdb6c2ccd
間中 祐介 <manaka.yusuke@nims.go.jp>
2021-11-11 09:17:15 +0900

    issue #15 により追加

---
+++ scripts/execute_remote_command_code_aster_execute.sh
@@ -0,0 +1,51 @@
+#!/bin/bash -x
+# code_aster SSH 実行スクリプト
+
+#sleep 10
+in_file=""
+out_file=""
+token=""
+NUMPROCS=4
+
+# 引数解釈
+filecount=1
+exec_path="/usr/local/bin/abaqus Job=XX interactive cpus=$NUMPROCS interactive mp_mode=threads"
+exec_dir=""
+infiles=("reduction_area.dat" "tesile_strength.dat" "yeild_strength.dat")
+count=0
+token=""
+instance_id=$0
+ssh_client="ssh -o ProxyCommand='ssh rme-u-tokyo nc $instance_id 50022' -i ~/id_rsa_cloud -p 50022 rme@"$instance_id
+rsync_command="ssh -o ProxyCommand='ssh rme-u-tokyo nc '$instance_id' 50022' -i /home/misystem/id_rsa_cloud -p 50022"
+client_cmd="~/misrc_remote_workflow/scripts/remote-side_scripts/code_aster_exec_ssh.sh"
+calc_dir=`cat /proc/sys/kernel/random/uuid`
+
+# リポジトリの展開
+rsync -av -L -e "$rsync_command" ~/assets/modules/misrc_remote_workflow/ rme@$instance_id:~/misrc_remote_workflow/ > /dev/null 2>&1
+
+# 作業ディレクトリ作成
+ssh_makedir=$ssh_client' "if [ -e /tmp/$calc_dir ]; then rm -rf /tmp/$calc_dir; fi; mkdir /tmp/$calc_dir"'
+eval $ssh_makedir
+#eval $ssh_client "if [ -e /tmp/$calc_dir ]; then rm -rf /tmp/$calc_dir; fi; mkdir /tmp/$calc_dir"
+# ディレクトリを転送
+rsync -av -L -e "$rsync_command" ./codeAster/ rme@$instance_id:/tmp/$calc_dir/codeAster/ > /dev/null 2>&1
+
+# プログラム実行
+echo `date '+%Y-%m-%d %T'`": start: code_aster: unknown" >> /home/misystem/assets/workflow/$MISYSTEM_SITEID/solver_logs/solver_execute.log
+ssh_remotecmd=$ssh_client' "cd /tmp/$calc_dir/codeAster; $client_cmd > /tmp/$calc_dir/codeAster/remote_program_exec.log 2>&1"'
+eval $ssh_remotecmd
+#ssh $ssh_client "cd /tmp/$calc_dir/codeAstere; $client_cmd > /tmp/$calc_dir/codeAster/remote_program_exec.log 2>&1"
+if [ $? != 0 ]; then
+    echo `date '+%Y-%m-%d %T'`": abnormal end: code_aster: unknown" >> /home/misystem/assets/workflow/site00002/solver_logs/solver_execute.log
+    rsync -av -e "$rsync_command" rme@$instance_id:/tmp/$calc_dir/codeAster/remote_program_exec.log ./
+    echo "---------- remote side log ----------------"
+    cat remote_program_exec.log
+    echo "---------- remote side log ----------------"
+    exit 1
+fi
+echo `date '+%Y-%m-%d %T'`": normal end: code_aster: unknown" >> /home/misystem/assets/workflow/site00002/solver_logs/solver_execute.log
+
+# 出力結果の取得
+rsync -av -e "$rsync_command" rme@$instalce_id:/tmp/$calc_dir/codeAster/ ./codeAster/
+
+exit 0
---
+++ scripts/remote-side_scripts/code_aster_exec_ssh.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+ASTER_HOME="/opt/salome_meca/V2018.0.1_public"
+source $ASTER_HOME/salome_prerequisites.sh
+source $ASTER_HOME/salome_modules.sh
+
+for var in "$@"
+do
+    echo "$var" >> exec.log
+    $ASTER_HOME/tools/Code_aster_frontend-201801/bin/as_run "$var" > /dev/null 2>&1 >> exec.log
+done
+
+echo "Finished Execution" >> exec.log
+cp -rlf exec.log weldparam.mess
+rm exec.log
+
